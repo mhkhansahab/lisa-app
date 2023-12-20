aaaaed341c5e51c9809807d089df8fe59a2c86b9
Hamza Khan Personal <hamza.khansahab@gmail.com>
2023-12-20 21:23:42 +0500

    demo title added for push test

--- lib/constant.dart
+++ lib/constant.dart
@@ -7,3 +7,4 @@ const chatHistoryTitle = "Control your chat history";
 const lisaInaccurateDesc = "LisaAI may provide inaccurate information about people, places, or facts.";
 const sensitiveInfoDesc = "Chats may be reviewed by our AI trainers to improve our systems.";
 const chatHistoryDesc = "Decide whether new chats on this device will appear in your chat history and be used to improve our systems.";
+const demoTitle = "Demo";
